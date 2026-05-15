c26d03986732c7dde428f86deff62055e78251be
v1zhu <vivianzhu.sd@gmail.com>
2026-05-15 00:57:34 -0700

    app bike details

--- map.js
+++ map.js
@@ -14,3 +14,23 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+map.on('load', async () => {
+  //code
+
+  map.addSource('boston_route', {
+  type: 'geojson',
+  data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+});
+
+map.addLayer({
+  id: 'bike-lanes',
+  type: 'line',
+  source: 'boston_route',
+  paint: {
+  'line-color': '#32D400',  // A bright green using hex code
+  'line-width': 5,          // Thicker lines
+  'line-opacity': 0.6       // Slightly less transparent
+}
+});
+});
