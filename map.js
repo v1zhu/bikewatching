6cb04ab2c45fee919895cff9248f6bd6716e4e46
v1zhu <vivianzhu.sd@gmail.com>
2026-05-15 01:47:22 -0700

    fixing dots

--- map.js
+++ map.js
@@ -99,7 +99,7 @@ map.on('moveend', updatePositions); // Final adjustment after movement ends
 
 
 function getCoords(station) {
-  const point = new mapboxgl.LngLat(+station.lon, +station.lat); // Convert lon/lat to Mapbox LngLat
+  const point = new mapboxgl.LngLat(+station.Lon, +station.Lat); // Convert lon/lat to Mapbox LngLat
   const { x, y } = map.project(point); // Project to pixel coordinates
   return { cx: x, cy: y }; // Return as object for use in SVG attributes
 }
