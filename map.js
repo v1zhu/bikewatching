06904b7ea5f15bc76b8855adb8431ef528b5114f
v1zhu <vivianzhu.sd@gmail.com>
2026-05-15 00:36:48 -0700

    add to map.js

--- map.js
+++ map.js
@@ -2,3 +2,15 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoidjF6aHUiLCJhIjoiY21wNmxnOW1oMGJ5MzJzb2wyeXV4cDR4aSJ9.guNKVth5v_2RZcsSiJYnhA';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
